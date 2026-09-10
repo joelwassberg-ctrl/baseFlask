25c4d8a1a30cafe6c3c9498eb76349413122e13e
Joel Wassberg <joel.wassberg@skola.taby.se>
2026-09-10 13:14:36 +0200

    lagt till och ändrat till index, contact och about us. samt gjort lite css

--- app.py
+++ app.py
@@ -6,16 +6,16 @@ such as headers, footers, and navigation menus.
 app = Flask(__name__)
 
 @app.route('/')
-def base():
-    return render_template('base.html')
+def index():
+    return render_template('index.html')
 
 @app.route('/about')
 def about():
     return render_template('about.html')
 
-@app.route('/home')
-def home():
-    return render_template('home.html')
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
